f80897de4acc2e04687a9ff820f5fa929b01f358
Simón Flores <asfloresmedrano@gmail.com>
2023-06-20 17:39:10 -0300

    Segunda entrega + login

--- src/DAO/models/users.model.js
+++ src/DAO/models/users.model.js
@@ -1,2 +1,12 @@
+import { Schema, model } from 'mongoose';
+
+const schema = new Schema({
+  first_name: { type: String, required: true, max: 100 },
+  last_name: { type: String, required: true, max: 100 },
+  email: { type: String, required: true, max: 100 },
+  age: { type: Number, required: true, unique: true },
+  password: { type: String, required: true, max: 100 },
+  isAdmin: { type: Boolean, required: true, max: 100 },
+});
 
 export const UserModel = model('users', schema);
