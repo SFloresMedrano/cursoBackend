1f6bbda971aabf69183ff04b50797f89f4083067
Simón Flores <asfloresmedrano@gmail.com>
2023-08-28 00:14:36 -0300

    Proceso de ticket

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,8 +1,14 @@
 import { messagesModelLogic } from '../DAO/mongo/messages.mongo.js';
 import { chatService } from '../services/chatService.js';
+import { logger } from '../utils.js';
+import CustomError from '../errors/customError.js';
+import errorsNum from '../errors/enum.js';
 
 class ChatController {
   async getMessages(req, res) {
+    if (!req.session.user) {
+        res.redirect('/api/sessions');     
+    }
     const chat = {};
     chat.messages = await chatService.getMessages();
     chat.isAdmin = req.session.user.role === 'admin' ? true : false;
